6c0a77c1c1c29dd13eec37e04613bb700d10a692
Zacky Burhani Hotib <zackyburhani99@gmail.com>
2019-06-12 10:32:44 +0700

    oauth

--- config/ttwitter.php
+++ config/ttwitter.php
@@ -14,8 +14,8 @@ return [
 	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
 	'USE_SSL'             => true,
 
-	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '') : '',
-	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', '') : '',
-	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '') : '',
-	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', '') : '',
+	'CONSUMER_KEY'        => function_exists('env') ? env('TWITTER_CONSUMER_KEY', '0ugFsXlrSxt1CcJBRGtDDLBy0') : '0ugFsXlrSxt1CcJBRGtDDLBy0',
+	'CONSUMER_SECRET'     => function_exists('env') ? env('TWITTER_CONSUMER_SECRET', 'zkW6YLIqEWHzCThLhwj5JCxEQxgMNPoN1SXn3Fgy4H17uCut7G') : 'zkW6YLIqEWHzCThLhwj5JCxEQxgMNPoN1SXn3Fgy4H17uCut7G',
+	'ACCESS_TOKEN'        => function_exists('env') ? env('TWITTER_ACCESS_TOKEN', '576221686-O0vh1Es3MSqNdaxRQTuVHZD7BxDkClByuxrNmM7R') : '576221686-O0vh1Es3MSqNdaxRQTuVHZD7BxDkClByuxrNmM7R',
+	'ACCESS_TOKEN_SECRET' => function_exists('env') ? env('TWITTER_ACCESS_TOKEN_SECRET', 'bhNKslaFOSwS37nNeHyrbNZs0Rk5VUVqxJK9RZDKwvsVC') : 'bhNKslaFOSwS37nNeHyrbNZs0Rk5VUVqxJK9RZDKwvsVC',
 ];
